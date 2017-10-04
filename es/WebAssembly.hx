a6c351a2fd39f40306fe6466946ce63da050b670
anon <anon@non.com>
2017-10-04 23:27:35 +0800

    WebAssembly rename

--- es/WebAssembly.hx
+++ es/WebAssembly.hx
@@ -9,33 +9,33 @@ private typedef Buffer = haxe.extern.EitherType<ArrayBuffer, js.html.ArrayBuffer
 /**
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WebAssembly
 
-`node --expose-wasm` for nodejs
+only for Browser
 */
 @:native("WebAssembly")
 extern class WebAssembly {
-	static function compile(buff: Buffer): Promise<Module>;
-	static function instantiate(buff: Buffer, ?importObject: Dynamic<Any>): Promise<{module: Module, instance: Instance}>;
+	static function compile(buff: Buffer): Promise<WasmModule>;
+	static function instantiate(buff: Buffer, ?importObject: Dynamic<Any>): Promise<{module: WasmModule, instance: WasmInstance}>;
 	static function validate(buff: Buffer): Bool;
 }
 
 @:native("WebAssembly.Module")
-extern class Module {
+extern class WasmModule {
 	function new(buff: Buffer);
 	function toString(): String;
-	static function customSections(mod: Module, sec_name: String): Array<ArrayBuffer>; // ???
-	static function exports(mod: Module): Array<{name: String, kind: String}>;
-	static function imports(mod: Module): Array<{name: String, kind: String, module: String}>;
+	static function customSections(mod: WasmModule, sec_name: String): Array<ArrayBuffer>; // ???
+	static function exports(mod: WasmModule): Array<{name: String, kind: String}>;
+	static function imports(mod: WasmModule): Array<{name: String, kind: String, module: String}>;
 }
 
 @:native("WebAssembly.Instance")
-extern class Instance {
+extern class WasmInstance {
 	var exports(default, null): Dynamic<Function>;
-	function new(mod: Module, ?importObject: Dynamic<Any>);
+	function new(mod: WasmModule, ?importObject: Dynamic<Any>);
 	function toString(): String;
 }
 
 @:native("WebAssembly.Memory")
-extern class Memory {
+extern class WasmMemory {
 	var buffer(default, null): ArrayBuffer;
 
 	function new(desc: {                          // each 1 is 64KB in size)
@@ -48,11 +48,11 @@ extern class Memory {
 }
 
 @:native("WebAssembly.Table")
-extern class Table {
+extern class WasmTable {
 	var length(default, null): Int;
 
 	function new(desc: {
-		element: TableElement,
+		element: WasmTableElement,
 		initial: Int,
 		?maximum: Int,
 	});
@@ -62,12 +62,12 @@ extern class Table {
 	function toString(): String;
 }
 
-@:enum private extern abstract TableElement(String) to String {
+@:enum private extern abstract WasmTableElement(String) to String {
 	var Anyfunc = "anyfunc";
 }
 
 @:native("WebAssembly.CompileError")
-extern class CompileError extends js.Error {
+extern class WasmCompileError extends js.Error {
 	var fileName(default, null): String;
 	var lineNumber(default, null): Int;
 	var columnNumber(default, null): Int;
@@ -75,7 +75,7 @@ extern class CompileError extends js.Error {
 }
 
 @:native("WebAssembly.LinkError")
-extern class LinkError extends js.Error {
+extern class WasmLinkError extends js.Error {
 	var fileName(default, null): String;
 	var lineNumber(default, null): Int;
 	var columnNumber(default, null): Int;
@@ -83,7 +83,7 @@ extern class LinkError extends js.Error {
 }
 
 @:native("WebAssembly.RuntimeError")
-extern class RuntimeError extends js.Error {
+extern class WasmRuntimeError extends js.Error {
 	var fileName(default, null): String;
 	var lineNumber(default, null): Int;
 	var columnNumber(default, null): Int;
